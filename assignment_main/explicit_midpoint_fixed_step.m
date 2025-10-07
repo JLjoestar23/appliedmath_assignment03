846e7bae3c55917a3c2e712e20366faf7550729b
Jojo Liu <34715258+JLjoestar23@users.noreply.github.com>
2025-10-07 15:54:22 -0400

    initial implementation of midpoint fixed step

---
+++ assignment_main/explicit_midpoint_fixed_step.m
@@ -0,0 +1,74 @@
+% Runs numerical integration using explicit midpoint approximation
+% 
+% INPUTS:
+% rate_func_in: the function used to compute dXdt. rate_func_in will
+% have the form: dXdt = rate_func_in(t,X) (t is before X)
+% tspan: a two element vector [t_start,t_end] that denotes the integration endpoints
+% X0: the vector describing the initial conditions, X(t_start)
+% h_ref: the desired value of the average step size (not the actual value)
+% 
+% OUTPUTS:
+% t_list: the vector of times, [t_start;t_1;t_2;...;.t_end] that X is approximated at
+% X_list: the vector of X, [X0';X1';X2';...;(X_end)'] at each time step
+% h_avg: the average step size
+% num_evals: total number of calls made to rate_func_in during the integration
+function [t_list, X_list, h_avg, num_evals] = explicit_midpoint_fixed_step(rate_func_in, tspan, X0, h_ref)
+    % calculate number of steps so that step size h is as close to h_ref as
+    % possible
+    N = floor((tspan(2) - tspan(1)) / h_ref);
+
+    % initialize step size
+    h_avg = (tspan(2) - tspan(1)) / N;
+
+    % initialize t_list based on start time, end time, and number of
+    % samples
+    t_list = linspace(tspan(1), tspan(2), N+1);
+    
+    % initialize solution list
+    X_list = zeros(length(t_list), length(X0));
+    X_list(1, :) = X0; % store initial condition
+
+    num_evals = 0; % initialize counter
+
+    for i=1:N
+        % evaluate XB at current time step using explicit midpoint
+        [XB, evals] = explicit_midpoint_step(rate_func_in, t_list(i), X_list(i, :), h_avg);
+        
+        % add to solution list
+        X_list(i+1, :) = XB;
+
+        % accumulate num_evals
+        num_evals = num_evals + evals;
+    end
+end
+
+% This function computes the value of X at the next time step using the 
+% explicit midpoint approximation
+%
+% INPUTS:
+% rate_func_in: the function used to compute dXdt. rate_func_in will
+% have the form: dXdt = rate_func_in(t,X) (t is before X)
+% t: the value of time at the current step
+% XA: the value of X(t)
+% h: the time increment for a single step i.e. delta_t = t_{n+1} - t_{n}
+%
+% OUTPUTS:
+% XB: the approximate value for X(t+h) (the next step)
+% formula depends on the integration method used
+% num_evals: A count of the number of times that you called
+% rate_func_in when computing the next step
+function [XB, num_evals] = explicit_midpoint_step(rate_func_in, t, XA, h)
+    % get current derivative from the input function at t and XA
+    dXdt_1 = rate_func_in(t, XA);
+
+    % evaluate XB at the midpoint
+    XB_half = XA + (h/2)*dXdt_1;
+    
+    % calculate derivative from the input function at the midpoint
+    dXdt_2 = rate_func_in(t + h/2, XB_half);
+    % evaluate XB
+    XB = XA + h*dXdt_2;
+    
+    % explicit midpoint requires 2 function calls
+    num_evals = 2;
+end
