5855a0edf6f5d6486f54a8e25001e3f8872b87b7
Jojo Liu <34715258+JLjoestar23@users.noreply.github.com>
2025-10-16 14:25:15 -0400

    cleaned up / reformatting

--- assignment_main/assignment03.m
+++ assignment_main/assignment03.m
@@ -1,4 +1,4 @@
-%% Testing Forward Euler Fixed Step on test function 1
+%% Forward Euler Fixed Step
 
 test_func_num = @rate_func01;
 test_func_analytical = @solution01;
@@ -23,32 +23,7 @@ grid on;
 xlim([t_list(1) t_list(end)])
 hold off;
 
-%% Testing Forward Euler Fixed Step method on test function 2
-
-test_func_num = @rate_func02;
-test_func_analytical = @solution02;
-
-tspan = [0; 3*pi];
-t = linspace(tspan(1), tspan(2), 500);
-X0 = [1, 0];
-h_ref = (0.1: 0.1: 0.4);
-
-figure();
-hold on;
-val = test_func_analytical(t);
-plot(t, val(1, :), 'b-', 'LineWidth', 2, 'DisplayName', 'Analytical');
-for i=1:length(h_ref)
-    [t_list, X_list, h_avg_1, num_evals_1] = forward_euler_fixed_step(test_func_num, tspan, X0, h_ref(i));
-    plot(t_list, X_list(:, 1), 'o-', 'MarkerSize', 4, 'LineWidth', 1, 'DisplayName', sprintf('h = %.2f', h_ref(i)));
-end
-
-title('Accuracy of Forward Euler Integration for Varying Step Sizes');
-legend();
-grid on;
-xlim([t_list(1) t_list(end)])
-hold off;
-
-%% Explicit Midpoint fixed step test on test function 1
+%% Explicit Midpoint fixed Step
 
 test_func_num = @rate_func01;
 test_func_analytical = @solution01;
@@ -73,31 +48,6 @@ grid on;
 xlim([t_list(1) t_list(end)])
 hold off;
 
-%% Testing Explicit Midpoint Method Fixed Step on test function 2
-
-test_func_num = @rate_func02;
-test_func_analytical = @solution02;
-
-tspan = [0; 3*pi];
-t = linspace(tspan(1), tspan(2), 500);
-X0 = [1, 0];
-h_ref = (0.1: 0.1: 0.4);
-
-figure();
-hold on;
-val = test_func_analytical(t);
-plot(t, val(1, :), 'b-', 'LineWidth', 2, 'DisplayName', 'Analytical');
-for i=1:length(h_ref)
-    [t_list, X_list, h_avg_1, num_evals_1] = explicit_midpoint_fixed_step(test_func_num, tspan, X0, h_ref(i));
-    plot(t_list, X_list(:, 1), 'o-', 'MarkerSize', 4, 'LineWidth', 1, 'DisplayName', sprintf('h = %.2f', h_ref(i)));
-end
-
-title('Accuracy of Explicit Midpoint Integration for Varying Step Sizes');
-legend();
-grid on;
-xlim([t_list(1) t_list(end)])
-hold off;
-
 %% Calculate and plot local truncation error for Forward Euler and Explicit Midpoint Method
 
 method_list = {'ForwardEuler', 'ExplicitMidpoint'};
@@ -114,11 +64,11 @@ method_list = {'ForwardEuler', 'ExplicitMidpoint'};
 rate_func = @rate_func01;
 analytical_soln = @solution01;
 tspan = [0, 2*pi];
-num_trials = 200;
+num_trials = 500;
 
 global_trunc_error_analysis(method_list, rate_func, analytical_soln, tspan, num_trials);
 
-%% Testing Implicit Euler Fixed Step method on test function 01
+%% Implicit Euler Fixed Step
 
 test_func_num = @rate_func01;
 test_func_analytical = @solution01;
@@ -144,36 +94,10 @@ grid on;
 xlim([t_list(1) t_list(end)])
 hold off;
 
-%% Testing Implicit Euler Fixed Step method on test function 02
+%% Implicit Midpoint Fixed Step
 
-test_func_num = @rate_func02;
-test_func_analytical = @solution02;
-step_func = @implicit_euler_step;
-
-tspan = [0; 3*pi];
-t = linspace(tspan(1), tspan(2), 500);
-X0 = [1, 0];
-h_ref = (0.1: 0.1: 0.4);
-
-figure();
-hold on;
-val = test_func_analytical(t);
-plot(t, val(1, :), 'b-', 'LineWidth', 2, 'DisplayName', 'Analytical');
-for i=1:length(h_ref)
-    [t_list, X_list, h_avg_1, num_evals_1] = fixed_step_integration(test_func_num, step_func, tspan, X0, h_ref(i));
-    plot(t_list, X_list(:, 1), 'o-', 'MarkerSize', 4, 'LineWidth', 1, 'DisplayName', sprintf('h = %.2f', h_ref(i)));
-end
-
-title('Accuracy of Implicit Euler Integration for Varying Step Sizes');
-legend();
-grid on;
-xlim([t_list(1) t_list(end)])
-hold off;
-
-%% Testing Implicit Midpoint Fixed Step method on test function 02
-
-test_func_num = @rate_func02;
-test_func_analytical = @solution02;
+test_func_num = @rate_func01;
+test_func_analytical = @solution01;
 step_func = @implicit_midpoint_step;
 
 tspan = [0; 3*pi];
@@ -190,7 +114,7 @@ for i=1:length(h_ref)
     plot(t_list, X_list(:, 1), 'o-', 'MarkerSize', 4, 'LineWidth', 1, 'DisplayName', sprintf('h = %.2f', h_ref(i)));
 end
 
-title('Accuracy of Implicit Midpoint Integration for Varying Step Sizes');
+title('Accuracy of Implicit Euler Integration for Varying Step Sizes');
 legend();
 grid on;
 xlim([t_list(1) t_list(end)])
